eff8f25084354543d0d3773a6be7a46ccd10e5ab
Alex Pankoff <ccntrq@screenri.de>
2017-07-29 10:08:10 +0200

    add rule generator

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,11 @@ module Main where
 import Data.List
 
 main :: IO ()
-main = print $ runGenerations (CellAutomaton startState rule30) 1000
+main = do
+  print $ runGenerations (CellAutomaton startState (genRule 30)) 10
+  print $ runGenerations (CellAutomaton startState (genRule  1)) 10
 
 startState = [[True]]
-
-
-
 
 type CellState = Bool
 type Generation = [CellState]
@@ -29,28 +28,44 @@ instance Show CellAutomaton where
 -- there are rules from 0 to rule 255.
 -- they are built according to the 8 different states
 
+genRule :: Int -> Rule
+genRule x
+ | x < 0 || x > 255 = error $ "valid rules are from 0 to 255"
+ | otherwise = rule
+ where rule neighborhood =
+         let results = decToBools x
+         in case neighborhood of
+             (True,  True,  True) -> results !! 0
+             (True,  True,  False)-> results !! 1
+             (True,  False, True) -> results !! 2
+             (True,  False, False)-> results !! 3
+             (False, True,  True) -> results !! 4
+             (False, True,  False)-> results !! 5
+             (False, False, True) -> results !! 6
+             (False, False, False)-> results !! 7
 
-rule30 :: Rule
-rule30 (True,  True,  True)  = False
-rule30 (True,  True,  False) = False
-rule30 (True,  False, True)  = False
-rule30 (True,  False, False) = True
-rule30 (False, True,  True)  = True
-rule30 (False, True,  False) = True
-rule30 (False, False, True)  = True
-rule30 (False, False, False) = False
+decToBools :: Int -> [Bool]
+decToBools x =
+  let res = decToBools' [] x
+  in pad res
+  where decToBools' acc 0 = acc
+        decToBools' acc x =  decToBools' (((x `mod` 2) == 1):acc) (x `div` 2)
+        pad xs
+          | length xs == 8 = xs
+          | otherwise = pad (False:xs)
+
 
 runGenerations automaton cnt
   | cnt > 0 = runGenerations (runAutomaton automaton) (cnt - 1)
   | otherwise = automaton
 
-runAutomaton automaton@(CellAutomaton state rule) = 
+runAutomaton automaton@(CellAutomaton state rule) =
   let generation = head state
       generation' = runGeneration rule generation
   in appendGeneration automaton generation'
 
 appendGeneration :: CellAutomaton -> Generation -> CellAutomaton
-appendGeneration automaton generation = 
+appendGeneration automaton generation =
   CellAutomaton (generation:(getState automaton)) (getRule automaton)
 
 runGeneration :: Rule -> Generation -> Generation
@@ -58,7 +73,7 @@ runGeneration rule generation = map rule $ getNeighborhoods generation
 
 getNeighborhoods :: Generation -> [Neighborhood]
 getNeighborhoods generation = getNeighborhoods' $ [False,False] ++ generation ++ [False,False]
-  where getNeighborhoods' generation = 
+  where getNeighborhoods' generation =
          let neighbors = take 3 generation
          in case neighbors of
            (a:b:c:[]) -> (a,b,c):(getNeighborhoods' $ drop 1 generation)
