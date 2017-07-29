a4d6fae409ecb4885532af16ed1e86c85c5294f1
Alex Pankoff <ccntrq@screenri.de>
2017-07-29 11:29:50 +0200

    Main: add funtion to write images for N generations for all rules

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,14 @@ import Automaton.Snapshot
 
 main :: IO ()
 main = do
-  print $ runGenerations (Automaton simpleStartState (mkRule 30)) 10
-  print $ runGenerations (Automaton emptyStartState (mkRule 30)) 10
-  print $ runGenerations (Automaton simpleStartState (mkRule 1)) 10
-  print $ runGenerations (Automaton emptyStartState (mkRule 1)) 10
-  writeSnapshot "./automaton.png" (runGenerations (Automaton simpleStartState (mkRule 30)) 100)
+  writeAllRules 100
+  print "wrote sample images of all rules to ./images"
+
+writeAllRules generations = mapM writeRule [0..255]
+ where writeRule x = writeSnapshot fileName $ runGenerations (Automaton simpleStartState (mkRule x)) generations
+         where fileName = "./images/" ++  (show generations)  ++ "/rule"  ++ ruleNum ++ ".png"
+               ruleNum = let str  = show x
+                         in case length str of
+                          1 -> "00" ++ str
+                          2 -> "0" ++ str
+                          3 -> str
