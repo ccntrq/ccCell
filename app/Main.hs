332882301da6c50c7b577b223a96c1bce8c49c50
Alex Pankoff <ccntrq@screenri.de>
2017-07-29 11:57:30 +0200

    Main: add dispatcher to allow creating images from cli

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,55 @@
 module Main where
 
+import System.Environment
+import System.Exit
+
 import Automaton
 import Automaton.Snapshot
 
+usage   = do
+             putStrLn "Usage: ccCell-exe [-h] generations rule?"
+             putStrLn ""
+             putStrLn "       ccCell - elementary cell automaton"
+             putStrLn ""
+             putStrLn "       given rule:"
+             putStrLn "       writes a png representation of that rule after 'generations'"
+             putStrLn "       generations to './images/generations/rule'"
+             putStrLn ""
+             putStrLn "       without rule:"
+             putStrLn "       creates an image for each rule"
+
 main :: IO ()
 main = do
-  writeAllRules 100
-  print "wrote sample images of all rules to ./images"
+       args <- getArgs
+       dispatch args
+       exit
 
-writeAllRules generations = mapM writeRule [0..255]
- where writeRule x = writeSnapshot fileName $ runGenerations (Automaton simpleStartState (mkRule x)) generations
-         where fileName = "./images/" ++  (show generations)  ++ "/rule"  ++ ruleNum ++ ".png"
-               ruleNum = let str  = show x
-                         in case length str of
-                          1 -> "00" ++ str
-                          2 -> "0" ++ str
-                          3 -> str
+
+dispatch ["-h"] = usage
+dispatch args =
+ case length args of
+   1 -> writeAllRules $ read (head args)
+   2 -> let generations = read (head args)
+            rule = read (args !! 1)
+        in writeRule generations rule
+   _ -> usage
+
+writeAllRules generations = do
+  mapM (writeRule generations) [0..255]
+  return ()
+
+writeRule generations rule =
+  writeSnapshot fileName $ runGenerations (Automaton simpleStartState (mkRule rule)) generations
+  where fileName = "./images/" ++  (show generations)  ++ "/rule"  ++ ruleNum ++ ".png"
+        ruleNum = let str  = show rule
+                  in case length str of
+                   1 -> "00" ++ str
+                   2 -> "0" ++ str
+                   3 -> str
+
+exit    = exitWith ExitSuccess
+
+-- TODO:
+--
+-- * chdir $FindBin::Bin
+-- * make output dir if it does not exist
